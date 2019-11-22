6667c3c17739d30db2f0bb74a6aa984391700862
RafikFarhad @m <rafikfarhad@gmail.com>
2019-11-23 02:24:17 +0600

    Yank Updated with updated deployment

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,12 @@
-YELLOW="\033[93m"
-RED="\033[91m"
-NORMAL="\033[0m"
-GREEN="\033[92m"
-BLINK="\033[5m"
+YELLOW="\e[0;40;93m"
+RED="\e[0;40;91m"
+NORMAL="\e[0;40;0m"
+GREEN="\e[0;40;92m"
+ORANGE="\e[0;40;33m"
+PURPLE="\e[0;40;96m"
+BLINK="\e[0;40;5m"
+
+echo -e "${YELLOW}Starting ${RED}Auto ${ORANGE} Environment${PURPLE} Setup${NORMAL}"
 
 prompt_install() {
 	echo -n "$1 is not installed. Would you like to install it? (y/n) " >&2
@@ -27,17 +31,17 @@ prompt_install() {
 }
 
 check_for_software() {
-	echo "${YELLOW}Checking to see if $1 is installed $NORMAL"
+	echo -e "${YELLOW}Checking to see if $1 is installed $NORMAL"
 	if ! [ -x "$(command -v $1)" ]; then
 		prompt_install $1
 	else
-		echo "${GREEN}$1 is installed $NORMAL"
+		echo -e "${GREEN}$1 is installed $NORMAL"
 	fi
 }
 
 check_default_shell() {
 	if [ -z "${SHELL##*zsh*}" ]; then
-		echo "${GREEN}Default shell is zsh $NORMAL"
+		echo -e "${GREEN}Default shell is zsh $NORMAL"
 	else
 		echo -n "Default shell is not zsh. Do you want to chsh -s \$(which zsh)? (y/n)"
 		old_stty_cfg=$(stty -g)
@@ -51,12 +55,13 @@ check_default_shell() {
 		fi
 	fi
 }
-
+echo -e "${PURPLE}"
 echo "The following task will be done:"
+echo -e "${ORANGE}"
 echo "⮕  Make sure that this device has zsh, vim and tmux installed"
 echo "⮕  Try to install missing packages"
 echo "⮕  Check the default shell and make the default shell to ZSH"
-echo
+echo -e "${NORMAL}"
 echo "Let's get started? (y/n)"
 old_stty_cfg=$(stty -g)
 stty raw -echo
@@ -94,11 +99,10 @@ fi
 
 cp $(pwd)/legacy/.service_manager.sh $HOME
 cp $(pwd)/legacy/.bash_aliases $HOME
-cp $(pwd)/legacy/yank.sh $HOME/.tmux/
 
-printf "source '$(pwd)/zshrc.conf' \nsource $HOME/.bash_aliases" >$HOME/.zshrc
-printf "source $(pwd)/vimrc.vim" >$HOME/.vimrc
-printf "source-file '$(pwd)/tmux.conf'" >$HOME/.tmux.conf
+printf "source $(pwd)/zshrc.conf \nsource $HOME/.bash_aliases" >$HOME/.zshrc
+printf "source $(pwd)/vimrc.conf" >$HOME/.vimrc
+printf "source-file $(pwd)/tmux.conf" >$HOME/.tmux.conf
 
 # Checking Tmux Plugin Manager
 if [ ! -e "$HOME/.tmux/plugins/tpm" ]; then
@@ -110,24 +114,26 @@ fi
 # Install TPM plugins.
 # TPM requires running tmux server, as soon as `tmux start-server` does not work
 # create dump __noop session in detached mode, and kill it when plugins are installed
-echo "Install TPM plugins\n"
+echo -e "${PURPLE}Installing TPM plugins"
+echo -e "${ORANGE}"
 tmux new -d -s __noop >/dev/null 2>&1 || true
 tmux set-environment -g TMUX_PLUGIN_MANAGER_PATH "$HOME/.tmux/plugins"
 "$HOME"/.tmux/plugins/tpm/bin/install_plugins || true
 tmux kill-session -t __noop >/dev/null 2>&1 || true
+echo -e "${NORMAL}"
 
 echo "Installing ZSH Plugins"
 if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-completions" ]; then
 	git clone https://github.com/zsh-users/zsh-completions $HOME/.oh-my-zsh/custom/plugins/zsh-completions
 else
-	echo "${GREEN}Updating ${YELLOW}zsh-completions ${GREEN}Plugin $NORMAL"
+	echo -e "${GREEN}Updating ${YELLOW}zsh-completions ${GREEN}Plugin $NORMAL"
 	cd "$HOME/.oh-my-zsh/custom/plugins/zsh-completions"
 	git pull
 fi
 if [ ! -d $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 else
-	echo "${GREEN}Updating ${YELLOW}zsh-syntax-highlighting ${GREEN}Plugin $NORMAL"
+	echo -e "${GREEN}Updating ${YELLOW}zsh-syntax-highlighting ${GREEN}Plugin $NORMAL"
 	cd "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
 	git pull
 fi
@@ -135,13 +141,13 @@ fi
 if [ ! -d $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]; then
 	git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 else
-	echo "${GREEN}Updating ${YELLOW}zsh-autosuggestions ${GREEN}Plugin $NORMAL"
+	echo -e "${GREEN}Updating ${YELLOW}zsh-autosuggestions ${GREEN}Plugin $NORMAL"
 	cd "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
 	git pull
 fi
 
 echo
-echo "${BLINK}${YELLOW}Please log out and log back in for default shell to be initialized $NORMAL"
+echo -e "${BLINK}${YELLOW}Please log out and log back in for default shell to be initialized $NORMAL"
 
 # for clbg in {40..47} {100..107} 49 ; do
 #         #Foreground
@@ -149,7 +155,7 @@ echo "${BLINK}${YELLOW}Please log out and log back in for default shell to be in
 #                 #Formatting
 #                 for attr in 0 1 2 4 5 7 ; do
 #                         #Print the result
-#                         echo -en "\e[${attr};${clbg};${clfg}m ^[${attr};${clbg};${clfg}m \e[0m"
+#                         echo -en "^[0;40;${attr};${clbg};${clfg}m ^[${attr};${clbg};${clfg}m ^[0;40;0m"
 #                 done
 #                 echo #Newline
 #         done
